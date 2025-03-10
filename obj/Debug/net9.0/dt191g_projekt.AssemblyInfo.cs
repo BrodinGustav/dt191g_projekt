f9c19eb95c335fb93ff21758d0f347f3f8b2370c
Gustav Brodin <brodin.gustav@gmail.com>
2025-03-10 14:17:49 +0100

    Commit - Justerat Index.cshmtl för Sanitation så tabell för saneringar syns på större enheter, och för mindre enheter syns aktuella saneringar som kort

--- obj/Debug/net9.0/dt191g_projekt.AssemblyInfo.cs
+++ obj/Debug/net9.0/dt191g_projekt.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dt191g_projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c3a6f9548944838aa43cec87dde185e03bab39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+291192f9b6dfd721f326a07be76f1a1f822595a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("dt191g_projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dt191g_projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
