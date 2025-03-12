67f1b532106aa1301243f6f324e20dac7cff007e
Gustav Brodin <brodin.gustav@gmail.com>
2025-03-12 11:32:16 +0100

    Commit - Justerat CSS - Lagt till kontroll så hamburgarmeny endast syns för inloggad

--- obj/Debug/net9.0/dt191g_projekt.AssemblyInfo.cs
+++ obj/Debug/net9.0/dt191g_projekt.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dt191g_projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130627218d3dd30c2c22bb7733468d46e1645d3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462f154f2af0a2a5e914c3154a4e8cdbe8b0ea79")]
 [assembly: System.Reflection.AssemblyProductAttribute("dt191g_projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dt191g_projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
